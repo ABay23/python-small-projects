913ee5052852e097ae57bd61d478e88e876a8b4c
ABay23 <alejandrobay23@gmail.com>
2023-07-27 23:46:05 -0400

    New Inputs and basic prog

--- todo_list_basic/main.py
+++ todo_list_basic/main.py
@@ -1,5 +1,22 @@
-print("Enter a todo:")
-text_inp = input()
-print(f'You have a new to do: {text_inp}!')
-print("Testing")
-print("Push")
+# print("Enter a todo:")
+# text_inp = input()
+# print(f'You have a new to do: {text_inp}!')
+
+# u_prompt = "Enter a To Do "
+
+# tod1 = input(u_prompt)
+# tod2 = input(u_prompt)
+# tod3 = input(u_prompt)
+
+# tods = [todo1, todo2, todo3]
+
+# print(tods)
+
+# print(type(tods))
+
+# Create a program that can get a title of a book and return the length of the title.
+intro = "Enter the title of your book: "
+book = str(input(intro))
+
+len = (len(book))
+print(f"The lenght of the title with spaces is {len}.")
