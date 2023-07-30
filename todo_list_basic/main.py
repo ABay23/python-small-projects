203c28d819f6ab95ae08daf5806bb030885edfd3
ABay23 <alejandrobay23@gmail.com>
2023-07-29 23:36:41 -0400

    Added for loop

--- todo_list_basic/main.py
+++ todo_list_basic/main.py
@@ -15,8 +15,30 @@
 # print(type(tods))
 
 # Create a program that can get a title of a book and return the length of the title.
-intro = "Enter the title of your book: "
-book = str(input(intro))
+# intro = "Enter the title of your book: "
+# book = str(input(intro))
 
-len = (len(book))
-print(f"The lenght of the title with spaces is {len}.")
+# len = (len(book))
+# print(f"The lenght of the title with spaces is {len}.")
+
+# List with a loop
+# user_prompt = "Enter a todo: "
+# todx= []
+
+# while True:
+#     todx = input(user_prompt)
+#     todxs.append(todx)
+#     print(todxs)
+
+tod = []
+
+while True:
+    user_input = input("Enter 'add' or 'stop'")
+    match user_input:
+        case 'add':
+            todx = input("Enter a todo: ")
+            tod.append(todx)
+        case 'stop':
+            print(tod)
+        case 'exit':
+            break
